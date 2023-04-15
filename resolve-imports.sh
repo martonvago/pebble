895bd14eaf79a4cb5ba8a257162b3a0d36db5120
mvago01 <mvago01@student.bbk.ac.uk>
2023-04-15 20:25:02 +0100

    PEB-11: rename resolve-imports flag

--- resolve-imports.sh
+++ resolve-imports.sh
@@ -3,16 +3,16 @@
 # Script to resolve / parameterise the path to the project 
 # root in imports in .tal files
 
-while getopts c opt; do
+while getopts x opt; do
    case $opt in
-     c ) committing=1                       ;;
-    \? ) echo "${0##*/} [ -c ]" >&2; exit 1 ;;
+     x ) undo=1                       ;;
+    \? ) echo "${0##*/} [ -x ]" >&2; exit 1 ;;
   esac
 done
 
 placeholder='<project-root>'
 
-if [[ "${committing}" -eq 1 ]]; 
+if [[ "${undo}" -eq 1 ]]; 
   then change="s|$PWD|$placeholder|g"; 
   else change="s|$placeholder|$PWD|g"; 
 fi
